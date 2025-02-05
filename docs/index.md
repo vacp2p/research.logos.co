11efea3109452f8af0c4860fb5df6c80113eefd3
seugu <99656002+seugu@users.noreply.github.com>
2025-02-05 11:19:57 +0300

    adding external_nullifier to the circuit

---
+++ docs/index.md
@@ -0,0 +1,11 @@
+---
+title: About Vac
+hide_table_of_contents: true
+---
+# About Vac
+
+Vac is a principle-driven research and development group that provides technical support to each [IFT](https://free.technology/) startup.
+Vac comprises [R&D Service Units](/vsus), [Deep Research](/deepresearch), and [Incubator Projects](/vips).
+We do applied research based on which we build protocols, libraries, [specifications](https://rfc.vac.dev/), and publications.
+As custodians of these protocols, our aim is to adhere to [a set of principles](/principles) that ensure their alignment with our core values and objectives.
+
---
+++ docusaurus.config.js
@@ -0,0 +1,170 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+require('dotenv').config()
+
+const math = require('remark-math')
+const katex = require('rehype-katex')
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'VacResearch',
+  url: 'https://vac.dev/',
+  baseUrl: '/',
+
+  markdown: {
+    mermaid: true,
+  },
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      '@acid-info/logos-docusaurus-preset',
+      /** @type {import('@acid-info/logos-docusaurus-preset').PluginOptions} */
+      ({
+        businessUnit: 'VacResearch',
+        theme: {
+          name: 'default',
+          options: {
+            customCss: [require.resolve('./src/css/custom.scss')],
+          },
+        },
+        docs: {
+          id: 'Docs',
+          breadcrumbs: false,
+          routeBasePath: '/',
+          sidebarPath: 'docs/sidebars.js',
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+        },
+        og: {},
+        generated: {
+          jobList: {
+            jobBoard: 'vac',
+          },
+        },
+      }),
+    ],
+  ],
+  plugins: [
+    [
+      '@docusaurus/plugin-content-blog',
+      /** @type {import('@docusaurus/plugin-content-blog').PluginOptions} */
+      ({
+        id: 'blog',
+        routeBasePath: '/rlog',
+        path: 'rlog',
+        blogTitle: 'Research Blog',
+        blogSidebarCount: 0,
+        authorsMapPath: 'authors.yml',
+        remarkPlugins: [math],
+        rehypePlugins: [katex],
+      }),
+    ],
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [{ from: '/research', to: '/rlog' }],
+        createRedirects(existingPath) {
+          return existingPath.startsWith('/rlog') && existingPath !== '/rlog'
+            ? [existingPath.replace('/rlog', '')]
+            : undefined
+        },
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@acid-info/logos-docusaurus-preset').ThemeConfig} */
+    ({
+      colorMode: {
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        items: [
+          {
+            type: 'search',
+          },
+          {
+            label: 'About Vac',
+            to: '/',
+            activeBaseRegex: '^/$',
+          },
+          {
+            label: 'Community',
+            to: '/community',
+            activeBasePath: 'community',
+          },
+          {
+            label: 'Research Blog',
+            to: '/rlog',
+            activeBasePath: 'rlog',
+          },
+          {
+            label: 'Join Us',
+            to: '/join-us',
+            activeBasePath: 'join-us',
+          },
+        ],
+      },
+      footer: {
+        links: [
+          {
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/vacp2p',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/PQFdubGt6d',
+              },
+              {
+                label: 'Github',
+                href: 'https://github.com/vacp2p',
+              },
+            ],
+          },
+          {
+            items: [
+              {
+                label: 'Work With Us',
+                href: 'https://jobs.status.im/',
+              },
+              {
+                label: 'Terms & Conditions',
+                to: '/terms',
+              },
+              {
+                href: '/privacy-policy',
+                label: 'Privacy Policy',
+              },
+              {
+                href: '/security',
+                label: 'Security',
+              },
+            ],
+          },
+        ],
+      },
+    }),
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+}
+
+module.exports = config
